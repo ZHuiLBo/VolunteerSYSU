43f4e58d0f673e040e1063c5a6f41d1a05094a10
zhaohui <24320132202527@stu.xmu.edu.cn>
2017-10-13 22:03:21 +0800

    添加AttendAction的相关方法和放置对应页面

--- Home/Lib/Action/AttendAction.class.php
+++ Home/Lib/Action/AttendAction.class.php
@@ -2,8 +2,24 @@
 // 参加活动
 class AttendAction extends Action {
 
-	  //查看所有活动列表
+	//查看所有活动列表
     public function getAllActiv(){
-		  $this->display();
+		$this->display();
     }
+
+    //查看活动详情
+    public function getActivInfo(){
+		$this->display('./Home/Tpl/Attend/activInfo.html');
+    }
+
+    //申请报名
+    public function applyActiv(){
+        $this->redirect('Attend/showApply');
+    }
+
+    //查看已报名列表
+    public function showApply(){
+        $this->display('./Home/Tpl/Attend/involvement.html');
+    }
+
 }
