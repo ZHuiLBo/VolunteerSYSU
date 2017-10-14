e4383725d1b6d593e6df917277a596a257bedf62
zhaohui <24320132202527@stu.xmu.edu.cn>
2017-10-14 20:26:54 +0800

    完成 修改电话和密码时的自动验证和密码加密

--- Home/Lib/Action/InfoAction.class.php
+++ Home/Lib/Action/InfoAction.class.php
@@ -27,27 +27,25 @@ class InfoAction extends Action {
 		
 	//修改个人信息
 	public function editInfo(){
-		//获取用户的id
-		$id=$_SESSION['user_id'];
-		//获得用户修改后的值
-		$phone=$_POST['phone'];
-		$pw=$_POST['pw'];
-		//修改数据库
-		$User=M('User');//实例化User对象
-		//要修改的数据对象属性赋值
-		$data['phone_number']=$phone;
-		$data['password']=$pw;
-		$where['user_id']=$id;
-		$result = $User->where($where)->save($data);//根据条件保存修改的数据
 
-		if($result){
-			//$returnData['status']='true';
-			$returnData['content']='修改成功!';
+		$data['phone_number']=$_POST['phone'];
+		if($_POST['pw']!=$_POST['oldpw'])//只有当新密码不等于旧密码时，才需要更新密码
+			$data['password']=$_POST['pw'];
+		$data['user_id']=$_SESSION['user_id'];
+		$User=D('User');//实例化User对象		
+		if(!$User->create($data)){
+			//如果创建失败 表示验证没通过 输出错误提示信息
+			$returnData['content']=$User->getError();
 		}else{
-			//$returnData['status']='false';
-			$returnData['content']='修改失败!';
+			//验证通过
+			$saveResult=$User->save();
+			if($saveResult!=false){
+				$returnData['content']='修改成功!';
+			}else{
+				$returnData['content']='修改失败!';
+			}
 		}
-		$this->ajaxReturn($returnData,'json');	
+		$this->ajaxReturn($returnData,'json');
 	}
 
 }
---
+++ Home/Lib/Model/UserModel.class.php
@@ -0,0 +1,22 @@
+<?php
+	class UserModel extends Model{
+
+		protected $_validate=array(
+			array('phone_number','/^1[3|4|5|8][0-9]\d{4,8}$/','手机号码错误！',1,'regex',3),
+			array('password','require','密码不能为空！',1,'',3),
+		);
+
+		protected $_auto=array(
+			array('password','md5',3,'function'),//在新增或者更新时进行md5加密 
+		);
+
+		/*
+		protected function checkCode($code){
+			if(md5($code)!=$_SESSION['code']){
+				return false;
+			}else{
+				return true;
+			}
+		}*/
+	}
+?>
