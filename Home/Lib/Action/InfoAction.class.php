dd36af40aedc42550d28529f5a7dde0fffc1a418
zhaohui <24320132202527@stu.xmu.edu.cn>
2017-10-14 17:42:24 +0800

    完成修改个人信息功能，但是没有对密码进行md5加密

--- Home/Lib/Action/InfoAction.class.php
+++ Home/Lib/Action/InfoAction.class.php
@@ -27,10 +27,27 @@ class InfoAction extends Action {
 		
 	//修改个人信息
 	public function editInfo(){
+		//获取用户的id
+		$id=$_SESSION['user_id'];
+		//获得用户修改后的值
 		$phone=$_POST['phone'];
 		$pw=$_POST['pw'];
-		$this->ajaxReturn($pw);
-		//$this->redirect('Info/showInfo');
+		//修改数据库
+		$User=M('User');//实例化User对象
+		//要修改的数据对象属性赋值
+		$data['phone_number']=$phone;
+		$data['password']=$pw;
+		$where['user_id']=$id;
+		$result = $User->where($where)->save($data);//根据条件保存修改的数据
+
+		if($result){
+			//$returnData['status']='true';
+			$returnData['content']='修改成功!';
+		}else{
+			//$returnData['status']='false';
+			$returnData['content']='修改失败!';
+		}
+		$this->ajaxReturn($returnData,'json');	
 	}
 
 }
