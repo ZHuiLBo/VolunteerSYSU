5393880dd801e625dd26729b0a2701d317289566
zhaohui <24320132202527@stu.xmu.edu.cn>
2017-10-30 19:20:51 +0800

    header.html加入判断：如果是管理员，则不出现“设置” 完成管理员按审核情况查看所有发布的活动

--- Home/Lib/Action/AuditAction.class.php
+++ Home/Lib/Action/AuditAction.class.php
@@ -1,9 +1,29 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
+// 管理员审核
 class AuditAction extends Action {
     //主页，显示已审核，未审核，审核不通过的活动列表 
-    public function index(){
-        $this->display('./Home/Tpl/Audit/auditList.html');
+    public function auditList(){
+        if($_GET['selectAuditResult']!=null)
+            $where['audit_result']=$_GET['selectAuditResult'];
+        else
+            $where['audit_result']='0';
+        $ar=$where['audit_result'];
+        $intAr=$where['audit_result'];
+        if($ar=='0')
+            $ar='待审核';
+        elseif($ar=='1')
+            $ar='审核通过';
+        else
+            $ar='审核不通过';
+        $Activity=M('Activity');
+        if($intAr=='0')
+            $arr=$Activity->field('activity_id, title, publish_time, sponsor')->where($where)->order('publish_time asc')->select();
+        else
+            $arr=$Activity->field('activity_id, title, publish_time, sponsor')->where($where)->order('publish_time desc')->select();
+        $this->assign('activties',$arr);
+        $this->assign('ar',$ar);
+        $this->assign('intAr',$intAr);
+        $this->display();
     }
     //显示具体活动审核页
     public function auditActiv(){
