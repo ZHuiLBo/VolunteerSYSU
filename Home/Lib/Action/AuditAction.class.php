d32974541b14bc7930be38e75e3dff0b9067156d
zhaohui <24320132202527@stu.xmu.edu.cn>
2017-10-30 20:57:11 +0800

    完成 管理员所有功能（查看所有活动 及 审核活动）

--- Home/Lib/Action/AuditAction.class.php
+++ Home/Lib/Action/AuditAction.class.php
@@ -1,6 +1,7 @@
 <?php
 // 管理员审核
 class AuditAction extends Action {
+
     //主页，显示已审核，未审核，审核不通过的活动列表 
     public function auditList(){
         if($_GET['selectAuditResult']!=null)
@@ -25,12 +26,42 @@ class AuditAction extends Action {
         $this->assign('intAr',$intAr);
         $this->display();
     }
+
     //显示具体活动审核页
     public function auditActiv(){
+        if($_GET['activId']==null)
+            $this->error("没有取到活动编号哦！");
+        
+        $where['activity_id']=$_GET['activId'];
+        $Activity=M('Activity');
+        $activ=$Activity->where($where)->find();
+        $ar=$activ['audit_result'];
+        if($ar=='0')
+            $ar='待审核!';
+        elseif($ar=='1')
+            $ar='审核通过!';
+        else
+            $ar='审核不通过!';
+        $this->assign('activ',$activ);
+        $this->assign('ar',$ar);
         $this->display();
     }
+
     //审核
     public function audit(){
-        $this->display();
+        if($_POST['activity_id']==null || $_POST['pass']==null)
+            $this->error("没有取到活动编号和审核结果哦");
+        $where['activity_id']=$_POST['activity_id'];
+        $data['audit_result']=$_POST['pass'];
+        $data['publish_time']=date('Y-m-d H:i:s',time());
+        $data['audit_opinion']=$_POST['opinion'];
+        
+        $Activity=M('Activity');
+        $result=$Activity->where($where)->save($data);
+        if($result!=false)
+            $this->redirect('Audit/auditList');
+        else
+            $this->error("保存审核情况出错啦！");
+
     }
 }
