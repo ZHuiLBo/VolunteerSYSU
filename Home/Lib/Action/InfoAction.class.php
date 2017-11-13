14bd5e3c5082eafc23fccf1b75d6d0730d367cae
zhaohui <24320132202527@stu.xmu.edu.cn>
2017-11-13 14:16:55 +0800

    加入查看时长代码，还未测试

--- Home/Lib/Action/InfoAction.class.php
+++ Home/Lib/Action/InfoAction.class.php
@@ -22,7 +22,32 @@ class InfoAction extends Action {
 
     //查看时长
     public function showTime(){
-        $this->display();
+
+        $startDate=$_GET['start'];
+        $endDate=$_GET['end'];
+        if($startDate==null || $endDate==null){
+            $this->assign('flag',0);
+            $this->display();
+        }else{
+            $this->assign('startTime',$startDate);
+            $this->assign('endTime',$endDate);
+            $this->assign('flag',1);
+    
+            //得到学生信息
+            $where['user_id']=$_SESSION['user_id'];
+            $user=M('User');
+            $arr=$user->field('academy, name, username')->where($where)->find();
+            $this->assign('info',$arr);
+    
+            //这部分未测试
+            //得到该学生某段时间的服务时长
+            $where['performance']=1;//已完成
+            $where['activity_date'] = array(array('EGT',$startDate),array('ELT',$endDate),'AND');
+            $Application=M('Application');
+            $result=$Application->join('activity ON activity.activity_id = application.activity_id')->where($where)->field('title, activity_date, hours_a_day')->order('activity_date, desc')->select();
+            $this->assign('results',$result);
+            $this->display();
+        }
     }
   
     //修改个人信息
